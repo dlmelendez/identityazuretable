e7a58f50642c1c5b46e7e7a3ec3138fcba9242aa
dlmelendez <dave_melendez@hotmail.com>
2016-07-19 22:54:00 -0700

    Initial check in for the project site.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,29 @@
-console.log('This would be the main JS file.');
+///<script>
+
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "/content/index.html"
+    })
+    .when("/gettingstarted", {
+        templateUrl: "/content/gettingstarted.html"
+    })
+    .when("/walkthrough", {
+        templateUrl: "/content/walkthrough.html"
+    })
+    .when("/walkthroughcore", {
+        templateUrl: "/content/walkthroughcore.html"
+    })
+    .when("/ragrs", {
+        templateUrl: "/content/ragrs.html"
+    })
+    .when("/techoverview", {
+        templateUrl: "/content/techoverview.html"
+    })
+    .otherwise({
+        templateUrl : "/content/index.html"
+    });
+});
+
+///</script>
