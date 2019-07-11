7c3bb8362d7bf4b1d70f44bd86cfc30ebd53a8ba
Christos Charmatzis <xr_xarmatzis@outlook.com>
2019-07-11 09:29:44 +0300

    Added samplemvccore3 example using asp.net core mvc with Identity Pages

---
+++ sample/samplemvccore3/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ElCamino.AspNetCore.Identity.AzureTable;
+using ElCamino.AspNetCore.Identity.AzureTable.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace samplemvccore3.Data
+{
+    public class ApplicationDbContext : IdentityCloudContext
+    {
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        public ApplicationDbContext(IdentityConfiguration config) : base(config)
+        {
+        }
+    }
+}
