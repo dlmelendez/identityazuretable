5b849659c1d105c1b4a752490c0012a33423e4f1
Dave Melendez <dave_melendez@hotmail.com>
2019-10-03 21:57:43 -0700

    .net core 3.0 sample app

---
+++ sample/samplemvccore4/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using samplemvccore4.Data;
+
+[assembly: HostingStartup(typeof(samplemvccore4.Areas.Identity.IdentityHostingStartup))]
+namespace samplemvccore4.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
