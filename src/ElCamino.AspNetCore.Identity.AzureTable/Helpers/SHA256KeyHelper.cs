4b50e1ea9717574d5d01989c2fdadd58faed01a8
Dave Melendez <dave_melendez@hotmail.com>
2022-04-26 19:09:01 -0500

    license header

--- src/ElCamino.AspNetCore.Identity.AzureTable/Helpers/SHA256KeyHelper.cs
+++ src/ElCamino.AspNetCore.Identity.AzureTable/Helpers/SHA256KeyHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// MIT License Copyright 2020 (c) David Melendez. All rights reserved. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
