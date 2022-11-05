37cff0feb667cd8599062069081832373481fbcc
David Melendez <dave_melendez@hotmail.com>
2022-11-05 00:33:40 -0500

    revert redirect

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,56 +3,57 @@
 var app = angular.module("myApp", ["ngRoute", "projectControllers"]);
 app.config(function ($routeProvider) {
     $routeProvider
-    .when("/", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/"
-    })
-    .when("/gettingstarted", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/gettingstarted.html"
-    })
-    .when("/walkthrough-menu", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/walkthrough/index.html"
-    })
-    .when("/walkthrough", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/walkthrough/walkthrough.html"
-    })
-    .when("/walkthroughcore", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/walkthrough/walkthroughcore.html"
-    })
-    .when("/walkthroughcore2", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/walkthrough/walkthroughcore2.html"
-    })
-    .when("/walkthroughcore3", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/walkthrough/walkthroughcore3.html"
-    })
-    .when("/walkthroughcore4", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/walkthrough/walkthroughcore4.html"
-    })
-    .when("/walkthroughcore5", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/walkthrough/walkthroughcore5.html"
-    })
-    .when("/ragrs", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/ragrs.html"
-    })
-    .when("/techoverview-menu", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/techoverview/index.html"
-    })
-    .when("/techoverview", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/techoverview/techoverview.html"
-    })
-    .when("/techoverview2", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/techoverview/techoverview2.html"
-    })
-    .when("/techoverview3", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/techoverview/techoverview3.html"
-    })
-    .when("/migration", {
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/migration.html"
-    })
-    .otherwise({
-        redirectTo: "https://elcamino.cloud/projects/docs/identityazuretable/"
-    });
+        .when("/", {
+            templateUrl: "content/index.html"
+        })
+        .when("/gettingstarted", {
+            templateUrl: "content/gettingstarted.html"
+        })
+        .when("/walkthrough-menu", {
+            templateUrl: "content/walkthrough-menu.html"
+        })
+        .when("/walkthrough", {
+            templateUrl: "content/walkthrough.html"
+        })
+        .when("/walkthroughcore", {
+            templateUrl: "content/walkthroughcore.html"
+        })
+        .when("/walkthroughcore2", {
+            templateUrl: "content/walkthroughcore2.html"
+        })
+        .when("/walkthroughcore3", {
+            templateUrl: "content/walkthroughcore3.html"
+        })
+        .when("/walkthroughcore4", {
+            templateUrl: "content/walkthroughcore4.html"
+        })
+        .when("/walkthroughcore5", {
+            templateUrl: "content/walkthroughcore5.html"
+        })
+        .when("/ragrs", {
+            templateUrl: "content/ragrs.html"
+        })
+        .when("/techoverview-menu", {
+            templateUrl: "content/techoverview-menu.html"
+        })
+        .when("/techoverview", {
+            templateUrl: "content/techoverview.html"
+        })
+        .when("/techoverview2", {
+            templateUrl: "content/techoverview2.html"
+        })
+        .when("/techoverview3", {
+            templateUrl: "content/techoverview3.html"
+        })
+        .when("/migration", {
+            templateUrl: "content/migration.html",
+            controller: "migrationDetails"
+        })
+        .otherwise({
+            templateUrl: "content/index.html"
+        });
 
-   
+
 });
 
 
