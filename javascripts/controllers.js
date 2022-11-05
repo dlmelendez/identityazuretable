4ddee7b41f078a6075a3870db6be413533d87681
David Melendez <dave_melendez@hotmail.com>
2022-11-05 10:09:03 -0500

    remove content

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -1,12 +1,5 @@
 ﻿var projectControllers = angular.module('projectControllers', []);
 
-projectControllers.controller('migrationDetails', ['$scope', '$http',
-  function ($scope, $http) {
-      $http.get('https://raw.githubusercontent.com/dlmelendez/identityazuretable/master/src/ElCamino.Identity.AzureTable.DataUtility/help.txt').success(function (data) {
-          $scope.helptext = data;
-      });
-
-    }]);
 
 projectControllers.controller('appController', function ($rootScope, $location) {
 
