7d9a2a9bc72c82b9b242f6d2bf6e695e86331c53
martincostello <martin@martincostello.com>
2022-11-13 18:56:20 +0000

    Use IdentityUserClaim<T>.ToClaim()
    Use the `IdentityUserClaim<T>.ToClaim()` method to create `Claim` instances from claim entities so that overridden behaviours are observed.

--- src/ElCamino.AspNetCore.Identity.AzureTable/UserOnlyStore.cs
+++ src/ElCamino.AspNetCore.Identity.AzureTable/UserOnlyStore.cs
@@ -388,7 +388,7 @@ namespace ElCamino.AspNetCore.Identity.AzureTable
                 //1.7 Claim rowkey migration 
                 if (_keyHelper.GenerateRowKeyIdentityUserClaim(tclaim.ClaimType, tclaim.ClaimValue) == tclaim.RowKey)
                 {
-                    rClaims.Add(new Claim(tclaim.ClaimType, tclaim.ClaimValue));
+                    rClaims.Add(tclaim.ToClaim());
                 }
             }
 
