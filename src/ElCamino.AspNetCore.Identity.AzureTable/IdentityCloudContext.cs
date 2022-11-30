f640788ed5c71e83a5b79682fc10735904fce638
Matthew Steeples <matthew@ledgerscope.com>
2022-11-30 18:33:43 +0000

    Allow TokenCredentials to be passed in (supporting Managed Identities instead of Shared Access Keys)

--- src/ElCamino.AspNetCore.Identity.AzureTable/IdentityCloudContext.cs
+++ src/ElCamino.AspNetCore.Identity.AzureTable/IdentityCloudContext.cs
@@ -30,17 +30,42 @@ namespace ElCamino.AspNetCore.Identity.AzureTable
             }
 #endif
 
-            _client = new TableServiceClient(config.StorageConnectionString);
-            _indexTable = _client.GetTableClient(FormatTableNameWithPrefix(config!.TablePrefix, !string.IsNullOrWhiteSpace(config!.IndexTableName) ? config!.IndexTableName! : TableConstants.TableNames.IndexTable));
-            _roleTable = _client.GetTableClient(FormatTableNameWithPrefix(config!.TablePrefix, !string.IsNullOrWhiteSpace(config!.RoleTableName) ? config!.RoleTableName! : TableConstants.TableNames.RolesTable));
-            _userTable = _client.GetTableClient(FormatTableNameWithPrefix(config!.TablePrefix, !string.IsNullOrWhiteSpace(config!.UserTableName) ? config!.UserTableName! : TableConstants.TableNames.UsersTable));
+            if (string.IsNullOrEmpty(config.StorageConnectionString) && config.StorageConnectionUri == null)
+            {
+                throw new ArgumentNullException(nameof(config.StorageConnectionString), "Either StorageConnectionString or StorageConnectionUri are required");
+            }
+            else if (!string.IsNullOrEmpty(config.StorageConnectionString))
+            {
+                _client = new TableServiceClient(config.StorageConnectionString);
+
+                if (config.TokenCredential != null)
+                {
+                    //If we've been passed a TokenCredential we can use that instead of the credentials in the connection string
+                    _client = new TableServiceClient(_client.Uri, config.TokenCredential);
+                }
+            }
+            else // if (config.StorageConnectionUri != null)
+            {
+                if (config.TokenCredential == null)
+                {
+                    throw new ArgumentNullException(nameof(config.TokenCredential), "TokenCredential is required when Uri is specified");
+                }
+                else
+                {
+                    _client = new TableServiceClient(config.StorageConnectionUri, config.TokenCredential);
+                }
+            }
+
+            _indexTable = _client.GetTableClient(FormatTableNameWithPrefix(config.TablePrefix, !string.IsNullOrWhiteSpace(config.IndexTableName) ? config.IndexTableName : TableConstants.TableNames.IndexTable));
+            _roleTable = _client.GetTableClient(FormatTableNameWithPrefix(config.TablePrefix, !string.IsNullOrWhiteSpace(config.RoleTableName) ? config.RoleTableName : TableConstants.TableNames.RolesTable));
+            _userTable = _client.GetTableClient(FormatTableNameWithPrefix(config.TablePrefix, !string.IsNullOrWhiteSpace(config.UserTableName) ? config.UserTableName : TableConstants.TableNames.UsersTable));
         }
 
         private static string FormatTableNameWithPrefix(string? tablePrefix, string baseTableName)
         {
             if (!string.IsNullOrWhiteSpace(tablePrefix))
             {
-                return string.Format("{0}{1}", tablePrefix!, baseTableName);
+                return string.Format("{0}{1}", tablePrefix, baseTableName);
             }
             return baseTableName;
         }
