e55d2c74d33afa38f16cbeb0353ceeb51ba55a03
David Melendez <dave_melendez@hotmail.com>
2023-11-09 15:58:16 -0600

    tighten up cloud context ctor and members

--- src/ElCamino.AspNetCore.Identity.AzureTable/IdentityCloudContext.cs
+++ src/ElCamino.AspNetCore.Identity.AzureTable/IdentityCloudContext.cs
@@ -5,50 +5,64 @@ using ElCamino.AspNetCore.Identity.AzureTable.Model;
 
 namespace ElCamino.AspNetCore.Identity.AzureTable
 {
+    /// <summary>
+    /// Identity table storage access
+    /// </summary>
     public class IdentityCloudContext
     {
-        protected TableServiceClient _client;
-        protected IdentityConfiguration _config;
-        protected TableClient _roleTable;
-        protected TableClient _indexTable;
-        protected TableClient _userTable;
+        private readonly TableServiceClient _client;
+        private readonly TableClient _roleTable;
+        private readonly TableClient _indexTable;
+        private readonly TableClient _userTable;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        /// <summary>
+        /// Uses <see cref="IdentityConfiguration"/> to configure identity table storage access
+        /// </summary>
+        /// <param name="config">Accepts <see cref="IdentityConfiguration"/></param>
         public IdentityCloudContext(IdentityConfiguration config)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
+#if NET6_0_OR_GREATER
+            ArgumentNullException.ThrowIfNull(config, nameof(config));
+#else
             if (config is null)
             {
                 throw new ArgumentNullException(nameof(config));
             }
-            Initialize(config);
+#endif
+
+            _client = new TableServiceClient(config.StorageConnectionString);
+            _indexTable = _client.GetTableClient(FormatTableNameWithPrefix(config!.TablePrefix, !string.IsNullOrWhiteSpace(config!.IndexTableName) ? config!.IndexTableName! : TableConstants.TableNames.IndexTable));
+            _roleTable = _client.GetTableClient(FormatTableNameWithPrefix(config!.TablePrefix, !string.IsNullOrWhiteSpace(config!.RoleTableName) ? config!.RoleTableName! : TableConstants.TableNames.RolesTable));
+            _userTable = _client.GetTableClient(FormatTableNameWithPrefix(config!.TablePrefix, !string.IsNullOrWhiteSpace(config!.UserTableName) ? config!.UserTableName! : TableConstants.TableNames.UsersTable));
         }
 
-        protected virtual void Initialize(IdentityConfiguration config)
+        private static string FormatTableNameWithPrefix(string? tablePrefix, string baseTableName)
         {
-            _config = config;
-            _client = new TableServiceClient(_config.StorageConnectionString);
-
-            _indexTable = _client.GetTableClient(FormatTableNameWithPrefix(!string.IsNullOrWhiteSpace(_config?.IndexTableName) ? _config!.IndexTableName! : TableConstants.TableNames.IndexTable));
-            _roleTable = _client.GetTableClient(FormatTableNameWithPrefix(!string.IsNullOrWhiteSpace(_config?.RoleTableName) ? _config!.RoleTableName! : TableConstants.TableNames.RolesTable));
-            _userTable = _client.GetTableClient(FormatTableNameWithPrefix(!string.IsNullOrWhiteSpace(_config?.UserTableName) ? _config!.UserTableName! : TableConstants.TableNames.UsersTable));
-        }
-
-        private string FormatTableNameWithPrefix(string baseTableName)
-        {
-            if (!string.IsNullOrWhiteSpace(_config?.TablePrefix))
+            if (!string.IsNullOrWhiteSpace(tablePrefix))
             {
-                return string.Format("{0}{1}", _config!.TablePrefix, baseTableName);
+                return string.Format("{0}{1}", tablePrefix!, baseTableName);
             }
             return baseTableName;
         }
 
+        /// <summary>
+        /// Access Role table information
+        /// </summary>
         public TableClient RoleTable => _roleTable;
 
+        /// <summary>
+        /// Access User table information
+        /// </summary>
         public TableClient UserTable => _userTable;
 
+        /// <summary>
+        /// Access Index table information
+        /// </summary>
         public TableClient IndexTable => _indexTable;
 
+        /// <summary>
+        /// Table Service access
+        /// </summary>
         public TableServiceClient Client => _client;
     }
 }
