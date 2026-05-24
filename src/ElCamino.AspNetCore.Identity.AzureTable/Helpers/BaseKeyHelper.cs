cb6ca906d90f492159c4ed147ee46e6d2fc0e70a
David Melendez <dave_melendez@hotmail.com>
2026-05-24 13:59:42 -0500

    key version update 10.1

--- src/ElCamino.AspNetCore.Identity.AzureTable/Helpers/BaseKeyHelper.cs
+++ src/ElCamino.AspNetCore.Identity.AzureTable/Helpers/BaseKeyHelper.cs
@@ -186,7 +186,7 @@ namespace ElCamino.AspNetCore.Identity.AzureTable.Helpers
         }
 
         /// <inheritdoc/>
-        public double KeyVersion => 10.0;
+        public double KeyVersion => 10.1;
 
         /// <summary>
         /// Convert Key Data to a hex hash string
